cc206c2cf712c33a294c2b72f9037c6f62bd36e2
Olivia Osborn <olioz95@gmail.com>
2019-02-15 11:52:12 -0500

    added post requests

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -12,4 +12,31 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/:id", async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id)
+        if (action) {
+            res.status(200).json(action)
+        } else {
+            res.status(404).json({message: "Could not find action with that id"})
+        }
+    } catch (error) {
+        res.status(500).json({message: "Could not fetch action"})
+    }
+})
+
+router.post("/", async (req, res) => {
+    newAction = req.body
+    if (!newAction.project_id || !newAction.description || !newAction.notes) {
+        res.status(400).json({message: "Please enter a project id, description, and notes!"})
+    } else {
+        try {
+            const action = await Actions.insert(newAction)
+            res.status(201).json({action})
+        } catch (error) {
+            res.status(500).json({message: "there was an error adding your action"})
+        }
+    }
+})
+
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -12,4 +12,31 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/:id", async (req, res) => {
+    try {
+        const project = await Projects.get(req.params.id)
+        if (project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({message: "Could not find project with that id"})
+        }
+    } catch (error) {
+        res.status(500).json({message: "Could not fetch project"})
+    }
+})
+
+router.post("/", async (req, res) => {
+    newProject = req.body
+    if (!newProject.name || !newProject.description) {
+        res.status(400).json({message: "Please enter both a name and a description!"})
+    } else {
+        try {
+            const project = await Projects.insert(newProject)
+            res.status(201).json({project})
+        } catch (error) {
+            res.status(500).json({message: "there was an error adding your project"})
+        }
+    }
+})
+
 module.exports = router;
